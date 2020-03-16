69b6bf9466acaf2f08db675cc5954f3d333f76c5
Sushil <sushilt553@gmail.com>
2020-03-16 16:20:18 -0400

    Create user validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,59 @@
 const express = require("express");
 const router = express.Router();
+const validateSignupInput = require("../../validation/signup");
+const validateLoginInput = require("../../validation/login");
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+
+router.post("/signup", (req, res) => {
+  const { errors, isValid } = validateSignupInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ username: req.body.username }).then(user => {
+    if (user) {
+      // Use the validations to send the error
+      errors.username = "Username already exists";
+      return res.status(400).json(errors);
+    } else {
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+      });
+    }
+  });
+});
+
+router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ username }).then(user => {
+    if (!user) {
+      // Use the validations to send the error
+      errors.username = "Incorrect username/password";
+      return res.status(404).json(errors);
+    }
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        // And here:
+        errors.password = "Incorrect username/password";
+        return res.status(400).json(errors);
+      }
+    });
+  });
+});
 
 module.exports = router;
