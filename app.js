a7214d59556de323775d71b3e4b1acce1aae2f7c
Sushil <sushilt553@gmail.com>
2020-03-16 16:01:53 -0400

    Create user model

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
 const db = require("./config/keys").mongoURI;
+const bodyParser = require("body-parser");
+
+const users = require('./routes/api/users');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => res.send("Hello Friends!"));
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
