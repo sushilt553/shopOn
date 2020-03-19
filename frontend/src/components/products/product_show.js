edeaa6e98a0dc08dfbb7ed7f79dade4575d5a114
Sushil <sushilt553@gmail.com>
2020-03-19 17:23:37 -0400

    Product show fix

--- frontend/src/components/products/product_show.js
+++ frontend/src/components/products/product_show.js
@@ -12,7 +12,7 @@ class ProductShow extends React.Component {
   }
 
   componentDidMount(){
-    this.props.fetchProduct(this.props.product._id)
+    this.props.fetchProduct(this.props.match.params.id)
     this.props.fetchAllCategories();
   }
 
