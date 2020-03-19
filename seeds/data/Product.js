13039b6fd9213ff4235c2bb94ec7e109c1cd8446
Jennie R <jbiakcin@icloud.com>
2020-03-19 13:59:43 -0700

    Start product seed file

---
+++ seeds/data/Product.js
@@ -0,0 +1,49 @@
+module.exports = [
+  {
+    name: "Limm Yoga Bands",
+    price: 8.47,
+    description: "Set of 5, 12-inch Workout Bands - Bonus eBook, Manual, Online Videos & Handy Carry Bag",
+    category: "Yoga",
+    image_urls: ["https://shoponbucket.s3.amazonaws.com/yoga/limm_1.jpg", 
+      "https://shoponbucket.s3.amazonaws.com/yoga/limm_2.jpg", 
+      "https://shoponbucket.s3.amazonaws.com/yoga/limm_3.jpg", 
+      "https://shoponbucket.s3.amazonaws.com/yoga/limm_4.jpg", 
+      "https://shoponbucket.s3.amazonaws.com/yoga/limm_5.jpg"
+    ]
+  },
+  {
+    name: "Long Sleeve Tee",
+    price: 14.99,
+    description: "Champion LIFE Men's Cotton Long Sleeve Tee",
+    category: "Clothing",
+    image_urls: []
+  },
+  {
+    name: "Heart Pendant Necklace",
+    price: 33.99,
+    description: "Infinity Love Heart Pendant Necklace with Crystals",
+    category: "3",
+    image_urls: []
+  },
+  {
+    name: "Under Armour Running Shoes",
+    price: 64.99,
+    description: "Under Armour Men's Charged Assert 8 Running Shoes",
+    category: "4",
+    image_urls: []
+  },
+  {
+    name: "Paint",
+    price: 5.68,
+    description: "Crayola Washable Paint",
+    category: "5",
+    image_urls: []
+  },
+  {
+    name: "American Dirt",
+    price: 16.79,
+    description: "By Jeanine Cummins.  It's a story about a bookstore owner in Mexico.",
+    category: "1",
+    image_urls: []
+  },
+]
